88260ee33912cca47986bc43a071c66f45faee32
Rich Barton <rbart65@gmail.com>
2016-11-17 14:13:14 -0500

    added GroupMeBot class that posts 'hello world' on test chatroom

---
+++ ff_bot/ff_bot.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+
+class GroupMeBot(object):
+    '''Creates GroupMe Bot to send messages'''
+    def __init__(self, bot_id):
+        self.bot_id = bot_id
+
+    def __repr__(self):
+        return "GroupMeBot(%s)" % self.bot_id
+
+    def send_message(self, text):
+        '''Sends a message to the chatroom'''
+        template = {
+                    "bot_id": self.bot_id,
+                    "text": text,
+                    "attachments": []
+                    }
+
+        headers = {'content-type': 'application/json'}
+        r = requests.post("https://api.groupme.com/v3/bots/post",
+                          data=json.dumps(template), headers=headers)
+        return r
+
+
+def main():
+    bot = GroupMeBot("b280ed5fe3b1406d4198a1715a")
+    text = "Hello World!"
+    bot.send_message(text)
+
+if __name__ == '__main__':
+    main()
