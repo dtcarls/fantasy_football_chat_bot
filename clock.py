1d99bdfe2bca1d9009ce1ae10d9091a6b3bd707e
Dean Carlson <deantcarlson@gmail.com>
2017-07-27 11:10:22 -0400

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import ff_bot
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=5)
+ff_bot.main("get_scoreboard_short")
+
+'''@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')'''
+
+sched.start()
