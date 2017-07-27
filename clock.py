511bd8566b1bbbd1684654aadbb96fa9eee859c5
Dean Carlson <deantcarlson@gmail.com>
2017-07-27 13:00:22 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,9 +5,5 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job(bot_main("get_scoreboard_short"),'interval', seconds=30)
 
-
-"""@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')"""
 try:
     sched.start()
