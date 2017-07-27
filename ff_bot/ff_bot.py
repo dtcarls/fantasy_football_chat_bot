2b70313aad1157cfda69db6caa630f9f0001f8a0
Dean Carlson <deantcarlson@gmail.com>
2017-07-27 13:32:32 -0400

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
     bot_main("init")
     sched = BlockingScheduler()
     print('before')
-    sched.add_job(bot_main('get_scoreboard_short'),'interval', seconds=30)
+    sched.add_job(bot_main,args='get_scoreboard_short','interval',seconds=30,replace_existing=True)
     print('after')
 
     sched.start()
