fac43dda12d83034e10a260596f92af3155e76b8
Dean Carlson <deantcarlson@gmail.com>
2017-07-27 13:22:06 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,11 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-#from ff_bot import bot_main
-import ff_bot
+#from apscheduler.schedulers.blocking import BlockingScheduler
+#import ff_bot
 
-sched = BlockingScheduler()
+#sched = BlockingScheduler()
 
-print('before')
-sched.add_job(ff_bot.bot_main("get_scoreboard_short"),'interval', seconds=30)
-print('after')
+#print('before')
+#sched.add_job(ff_bot.bot_main("get_scoreboard_short"),'interval', seconds=30)
+#print('after')
 
-sched.start()
+#sched.start()
