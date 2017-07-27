a966b374d4d5d2da5f80c955c1f491b4e0de0d34
Dean Carlson <deantcarlson@gmail.com>
2017-07-27 14:12:33 -0400

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -118,11 +118,31 @@ def bot_main(function):
 
 
 if __name__ == '__main__':
+    ff_start_date = os.environ["START_DATE"]
+    if not ff_start_date
+        ff_start_date='2017-09-05'
+    ff_end_date = os.environ["END_DATE"]
+    if not ff_end_date
+        ff_end_date='2017-09-05'
     bot_main("init")
     sched = BlockingScheduler()
     print('before')
-    arg="get_scoreboard_short"
-    sched.add_job(bot_main,'interval',[arg],days=30,replace_existing=True)
+    sched.add_job(bot_main,'interval',['get_scoreboard_short'],seconds=30,replace_existing=True)
+    '''
+    so my planned schedule is power rankings go out tuesday evening. 
+    matchups go out thursday afternoon. score update thursday night. 
+    score update every hour on sunday from 1-11. 
+    close scores go out monday evening. 
+    score update monday night every hour from 8-11'''
+    ff_start_date='2017-07-27'
+    sched.add_job(bot_main, 'cron', ['get_power_rankings'], day_of_week='thu', hour=18, minute=14,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    
+    sched.add_job(bot_main, 'cron', ['get_power_rankings'], day_of_week='tue', hour=18, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_matchups'], day_of_week='thu', hour=19, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_close_scores'], day_of_week='mon', hour=18, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='fri,mon,tue', hour=0, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='sun', hour='13,16,20',start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='mon', hour='20',start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
     print('after')
 
     sched.start()
