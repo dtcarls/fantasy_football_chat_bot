9a2c42f8a3d167ffda122282339032f063ebd9f9
Dean Carlson <deantcarlson@gmail.com>
2017-08-03 14:48:15 -0400

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -144,6 +144,11 @@ if __name__ == '__main__':
     except:
         ff_end_date='2017-12-26'
 
+    try:
+        myTimezone = os.environ["TIMEZONE"]
+    except:
+        myTimezone='America/New_York'
+
     bot_main("init")
     sched = BlockingScheduler()
     '''
@@ -155,19 +160,19 @@ if __name__ == '__main__':
     '''
 
     '''sched.add_job(bot_main, 'interval', ['get_matchups'], seconds=30, id='get_matchups_test', replace_existing=True)'''
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='test', day_of_week='thu', hour='14', minute=45,timezone='America/New_York',replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='test', day_of_week='thu', hour='14', minute=50, timezone=myTimezone, replace_existing=True)
     
     '''EST'''
-    '''
-    sched.add_job(bot_main, 'cron', ['get_power_rankings'], day_of_week='tue', hour=18, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_matchups'], day_of_week='thu', hour=19, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_close_scores'], day_of_week='mon', hour=18, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='fri,mon,tue', hour=0, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='sun', hour='13,16,20',start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], day_of_week='mon', hour='20',start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-    '''
+    sched.add_job(bot_main, 'cron', ['get_power_rankings'], id='power_rankings', day_of_week='tue', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_matchups'], id='matchups', day_of_week='thu', hour=19, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores', day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1', day_of_week='fri,mon,tue', hour=0, minute=30, start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2', day_of_week='sun', hour='13,16,20', start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard3', day_of_week='mon', hour='20', start_date=ff_start_date, end_date=ff_end_date, timezone='America/New_York', replace_existing=True)
+    
 
     '''GMT/UTC'''
+    '''
     sched.add_job(bot_main, 'cron', ['get_power_rankings'], id='power_rankings', day_of_week='tue', hour='23', minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_matchups'], id='matchups', day_of_week='thu', hour=23, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores', day_of_week='mon', hour=23, minute=30,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
@@ -175,5 +180,5 @@ if __name__ == '__main__':
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2', day_of_week='sun', hour='18,21',start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard3', day_of_week='mon', hour=1,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard4', day_of_week='tue', hour=1,start_date=ff_start_date,end_date=ff_end_date,replace_existing=True)
-
+    '''
     sched.start()
