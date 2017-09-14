b1068eb95ec1fb08ca21afa27e03990e6a8d4478
Dean Carlson <deantcarlson@gmail.com>
2017-09-14 19:54:46 -0400

    Update ff_bot.py

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -68,7 +68,7 @@ def get_matchups(league):
     score = ['%s(%s-%s) vs %s(%s-%s)' % (i.home_team.team_name, i.home_team.wins, i.home_team.losses,
              i.away_team.team_name, i.away_team.wins, i.away_team.losses) for i in matchups
              if i.away_team]
-    text = ['This Week\'s Matchups'] + score + '\n\n' + random_phrase()
+    text = ['This Week\'s Matchups'] + score + '\n' + random_phrase()
     return '\n'.join(text)
 
 def get_close_scores(league):
