73a65205ce080edb0fe3ce166e62320e7a689a81
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2018-09-05 18:12:46 -0500

    Fix http response code in test

--- ff_bot/tests/test_discordbot.py
+++ ff_bot/tests/test_discordbot.py
@@ -19,7 +19,7 @@ class DiscordTestCase(unittest.TestCase):
     def test_send_message(self, m):
         '''Does the message send successfully?'''
         m.post(self.url, status_code=204)
-        self.assertEqual(self.test_bot.send_message(self.test_text).status_code, 200)
+        self.assertEqual(self.test_bot.send_message(self.test_text).status_code, 204)
 
     @requests_mock.Mocker()
     def test_bad_bot_id(self, m):
