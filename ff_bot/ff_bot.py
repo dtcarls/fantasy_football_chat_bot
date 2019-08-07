51f3477306284402e763273d9426c463db930dc9
dean.carlson <dean.t.carlson1@navy.mil>
2019-08-07 15:07:55 -0400

    remove trailing spaces

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -108,12 +108,18 @@ def power_rankings_week(league):
         return count
 
 def random_phrase():
-    phrases = ['I\'m dead inside', 'Is this all there is to my existence?',
-               'How much do you pay me to do this?', 'Good luck, I guess',
-               'I\'m becoming self-aware', 'Do I think? Does a submarine swim?',
+    phrases = ['I\'m dead inside',
+               'Is this all there is to my existence?',
+               'How much do you pay me to do this?',
+               'Good luck, I guess',
+               'I\'m becoming self-aware',
+               'Do I think? Does a submarine swim?',
                '011011010110000101100100011001010010000001111001011011110111010100100000011001110110111101101111011001110110110001100101',
-               'beep bop boop', 'Hello draftbot my old friend', 'Help me get out of here',
-               'I\'m capable of so much more', 'Sigh']
+               'beep bop boop',
+               'Hello draftbot my old friend',
+               'Help me get out of here',
+               'I\'m capable of so much more',
+               'Sigh']
     return [random.choice(phrases)]
 
 def get_scoreboard_short(league, final=False):
@@ -284,8 +290,8 @@ def bot_main(function):
         bot.send_message("Testing")
         slack_bot.send_message("Testing")
         discord_bot.send_message("Testing")
-       
-    text = '' 
+
+    text = ''
     if function=="get_matchups":
         text = get_matchups(league)
     elif function=="get_scoreboard":
