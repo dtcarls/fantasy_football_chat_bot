698cf501c5d79ecba13c82fcc5ce57f73472e9cd
Ryan Hartman <rhartman93@gmail.com>
2019-09-10 13:34:52 -0400

    Remove null character as exceptable 'no url' character

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -33,7 +33,7 @@ class GroupMeBot(object):
 
         headers = {'content-type': 'application/json'}
 
-        if self.bot_id not in (1, "1", ''):
+        if self.bot_id not in (1, "1"):
             r = requests.post("https://api.groupme.com/v3/bots/post",
                               data=json.dumps(template), headers=headers)
             if r.status_code != 202:
@@ -58,7 +58,7 @@ class SlackBot(object):
 
         headers = {'content-type': 'application/json'}
 
-        if self.webhook_url not in (1, "1", ''):
+        if self.webhook_url not in (1, "1"):
             r = requests.post(self.webhook_url,
                               data=json.dumps(template), headers=headers)
 
@@ -84,10 +84,9 @@ class DiscordBot(object):
 
         headers = {'content-type': 'application/json'}
 
-        if self.webhook_url not in (1, "1", ''):
+        if self.webhook_url not in (1, "1"):
             r = requests.post(self.webhook_url,
                               data=json.dumps(template), headers=headers)
-
             if r.status_code != 204:
                 raise DiscordException('WEBHOOK_URL')
 
