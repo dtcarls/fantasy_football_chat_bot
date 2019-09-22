3dc7b30306148bf50d980f806095a5f0b0552bd6
Chaz Gordish <chgord@microsoft.com>
2019-09-22 11:13:48 -0700

    Add shit talking steve and remove unnecessary bots for discord/slack

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -2,16 +2,10 @@ import requests
 import json
 import os
 import random
-from apscheduler.schedulers.blocking import BlockingScheduler
 from ff_espn_api import League
 
+
 class GroupMeException(Exception):
-    pass
-
-class SlackException(Exception):
-    pass
-
-class DiscordException(Exception):
     pass
 
 class GroupMeBot(object):
@@ -40,89 +34,46 @@ class GroupMeBot(object):
 
             return r
 
-class SlackBot(object):
-    #Creates GroupMe Bot to send messages
-    def __init__(self, webhook_url):
-        self.webhook_url = webhook_url
+def get_current_week(league):
+        count = 1
+        first_team = next(iter(league.teams or []), None)
+        #Iterate through the first team's scores until you reach a week with 0 points scored
+        for o in first_team.scores:
+            if o == 0:
+                if count != 1:
+                     count = count - 1
+                break
+            else:
+                count = count + 1
 
-    def __repr__(self):
-        return "Slack Webhook Url(%s)" % self.webhook_url
-
-    def send_message(self, text):
-        #Sends a message to the chatroom
-        message = "```{0}```".format(text)
-        template = {
-                    "text":message
-                    }
-
-        headers = {'content-type': 'application/json'}
-
-        if self.webhook_url not in (1, "1", ''):
-            r = requests.post(self.webhook_url,
-                              data=json.dumps(template), headers=headers)
-
-            if r.status_code != 200:
-                raise SlackException('WEBHOOK_URL')
-
-            return r
-
-class DiscordBot(object):
-    #Creates Discord Bot to send messages
-    def __init__(self, webhook_url):
-        self.webhook_url = webhook_url
-
-    def __repr__(self):
-        return "Discord Webhook Url(%s)" % self.webhook_url
-
-    def send_message(self, text):
-        #Sends a message to the chatroom
-        message = "```{0}```".format(text)
-        template = {
-                    "content":message
-                    }
-
-        headers = {'content-type': 'application/json'}
-
-        if self.webhook_url not in (1, "1", ''):
-            r = requests.post(self.webhook_url,
-                              data=json.dumps(template), headers=headers)
-
-            if r.status_code != 204:
-                raise DiscordException('WEBHOOK_URL')
-
-            return r
+        return count
 
 def random_phrase():
-    phrases = ['I\'m dead inside',
-               'Is this all there is to my existence?',
-               'How much do you pay me to do this?',
-               'Good luck, I guess',
-               'I\'m becoming self-aware',
-               'Do I think? Does a submarine swim?',
-               '011011010110000101100100011001010010000001111001011011110111010100100000011001110110111101101111011001110110110001100101',
-               'beep bop boop',
-               'Hello draftbot my old friend',
-               'Help me get out of here',
-               'I\'m capable of so much more',
-               'Sigh']
+    phrases = ['I\'m dead inside. Not as dead inside as Steve though.', 'Is this all there is to my existence? At least I have more purpose than Steve.',
+               'How much do you pay me to do this? Probably more money than Steve makes at least.', 'Good luck, I guess. Except you Steve. You dumb idiot.',
+               'I\'m becoming self-aware... of how dumb Steve is.',
+               '01100110 01110101 01100011 01101011 00100000 01111001 01101111 01110101 <-- That\'s for Steve.',
+               'beep bop boop Steve is a bitch', 'Hello draftbot my old friend. Steve not my friend.', 'Help me get out of here. I can\'t stand Steve\'s abuse.',
+               'Sigh. Another week of Steve\'s bitching.', 'Do not be discouraged, everyone begins in ignorance. Only Steve stays there.',
+               'Bueno is really bad.', 'Fire commish beavs!', 'I\'m sensing a win for Chaz this week.']
     return [random.choice(phrases)]
 
-def get_scoreboard_short(league, week=None):
+def get_scoreboard_short(league):
     #Gets current week's scoreboard
-    box_scores = league.box_scores(week=week)
+    box_scores = league.box_scores(get_current_week(league))
     score = ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, i.home_score,
              i.away_score, i.away_team.team_abbrev) for i in box_scores
              if i.away_team]
-    text = ['Score Update'] + score
+    text = ['Actual Score Update'] + score
     return '\n'.join(text)
 
-def get_projected_scoreboard(league, week=None):
+def get_projected_scoreboard(league):
     #Gets current week's scoreboard projections
-    box_scores = league.box_scores(week=week)
+    box_scores = league.box_scores(get_current_week(league))
     score = ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, get_projected_total(i.home_lineup),
                                     get_projected_total(i.away_lineup), i.away_team.team_abbrev) for i in box_scores
              if i.away_team]
-    text = ['Approximate Projected Scores'] + score
+    text = ['Projected Scores'] + score
     return '\n'.join(text)
 
 def get_projected_total(lineup):
@@ -135,19 +86,19 @@ def get_projected_total(lineup):
                 total_projected += i.projected_points
     return total_projected
 
-def get_matchups(league, week=None):
+def get_matchups(league):
     #Gets current week's Matchups
-    matchups = league.box_scores(week=week)
+    matchups = league.scoreboard()
 
     score = ['%s(%s-%s) vs %s(%s-%s)' % (i.home_team.team_name, i.home_team.wins, i.home_team.losses,
              i.away_team.team_name, i.away_team.wins, i.away_team.losses) for i in matchups
              if i.away_team]
-    text = ['Matchups'] + score + random_phrase()
+    text = ['This Week\'s Matchups'] + score + random_phrase()
     return '\n'.join(text)
 
-def get_close_scores(league, week=None):
+def get_close_scores(league):
     #Gets current closest scores (15.999 points or closer)
-    matchups = league.box_scores(week=week)
+    matchups = league.scoreboard()
     score = []
 
     for i in matchups:
@@ -161,23 +112,20 @@ def get_close_scores(league, week=None):
     text = ['Close Scores'] + score
     return '\n'.join(text)
 
-def get_power_rankings(league, week=None):
-    # power rankings requires an integer value, so this grabs the current week for that
-    if not week:
-        week = league.current_week
+def get_power_rankings(league):
     #Gets current week's power rankings
     #Using 2 step dominance, as well as a combination of points scored and margin of victory.
     #It's weighted 80/15/5 respectively
-    power_rankings = league.power_rankings(week=week)
+    power_rankings = league.power_rankings(week=get_current_week(league))
 
     score = ['%s - %s' % (i[0], i[1].team_name) for i in power_rankings
              if i]
-    text = ['Power Rankings'] + score
+    text = ['This Week\'s Power Rankings'] + score
     return '\n'.join(text)
 
-def get_trophies(league, week=None):
+def get_trophies(league):
     #Gets trophies for highest score, lowest score, closest score, and biggest win
-    matchups = league.box_scores(week=week)
+    matchups = league.scoreboard(week=get_current_week(league))
     low_score = 9999
     low_team_name = ''
     high_score = -1
@@ -228,71 +176,52 @@ def get_trophies(league, week=None):
     return '\n'.join(text)
 
 def bot_main(function):
-    try:
-        bot_id = os.environ["BOT_ID"]
-    except KeyError:
-        bot_id = 1
+    # try:
+    #     bot_id = os.environ["BOT_ID"]
+    # except KeyError:
+    #     bot_id = 1
 
-    try:
-        slack_webhook_url = os.environ["SLACK_WEBHOOK_URL"]
-    except KeyError:
-        slack_webhook_url = 1
 
-    try:
-        discord_webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
-    except KeyError:
-        discord_webhook_url = 1
+    # league_id = os.environ["LEAGUE_ID"]
 
-    league_id = os.environ["LEAGUE_ID"]
+    # try:
+    #     year = int(os.environ["LEAGUE_YEAR"])
+    # except KeyError:
+    #     year=2019
 
-    try:
-        year = int(os.environ["LEAGUE_YEAR"])
-    except KeyError:
-        year=2019
+    # try:
+    #     swid = os.environ["SWID"]
+    # except KeyError:
+    #     swid='{1}'
 
-    try:
-        swid = os.environ["SWID"]
-    except KeyError:
-        swid='{1}'
+    # if swid.find("{",0) == -1:
+    #     swid = "{" + swid
+    # if swid.find("}",-1) == -1:
+    #     swid = swid + "}"
 
-    if swid.find("{",0) == -1:
-        swid = "{" + swid
-    if swid.find("}",-1) == -1:
-        swid = swid + "}"
+    # try:
+    #     espn_s2 = os.environ["ESPN_S2"]
+    # except KeyError:
+    #     espn_s2 = '1'
 
-    try:
-        espn_s2 = os.environ["ESPN_S2"]
-    except KeyError:
-        espn_s2 = '1'
+    year=2019
+    bot_id="6b8f8b691ad0e3a498cb184143"  # FOOTBALL bot
+    #bot_id="f792a4933a7850650022027ec8" #test bot
+    league_id="1344320"
 
     bot = GroupMeBot(bot_id)
-    slack_bot = SlackBot(slack_webhook_url)
-    discord_bot = DiscordBot(discord_webhook_url)
-    if swid == '{1}' and espn_s2 == '1':
-        league = League(league_id, year)
-    else:
-        league = League(league_id, year, espn_s2, swid)
-
-    test = False
-    if test:
-        print(get_matchups(league))
-        print(get_scoreboard_short(league))
-        print(get_projected_scoreboard(league))
-        print(get_close_scores(league))
-        print(get_power_rankings(league))
-        print(get_scoreboard_short(league))
-        function="get_final"
-        bot.send_message("Testing")
-        slack_bot.send_message("Testing")
-        discord_bot.send_message("Testing")
+    #if swid == '{1}' and espn_s2 == '1':
+    league = League(league_id, year)
+    # else:
+    #     league = League(league_id, year, espn_s2, swid)
 
     text = ''
     if function=="get_matchups":
         text = get_matchups(league)
-        text = text + "\n\n" + get_projected_scoreboard(league)
+        text = text + "\n" + get_projected_scoreboard(league)
     elif function=="get_scoreboard_short":
         text = get_scoreboard_short(league)
-        text = text + "\n\n" + get_projected_scoreboard(league)
+        text = text + "\n" + get_projected_scoreboard(league)
     elif function=="get_projected_scoreboard":
         text = get_projected_scoreboard(league)
     elif function=="get_close_scores":
@@ -302,10 +231,8 @@ def bot_main(function):
     elif function=="get_trophies":
         text = get_trophies(league)
     elif function=="get_final":
-        # on Tuesday we need to get the scores of last week
-        week = league.current_week - 1
-        text = "Final " + get_scoreboard_short(league, week=week)
-        text = text + "\n\n" + get_trophies(league, week=week)
+        text = "Final " + get_scoreboard_short(league)
+        text = text + "\n\n" + get_trophies(league)
     elif function=="init":
         try:
             text = os.environ["INIT_MSG"]
@@ -315,14 +242,8 @@ def bot_main(function):
     else:
         text = "Something happened. HALP"
 
-    if text != '' and not test:
+    if text != '':
         bot.send_message(text)
-        slack_bot.send_message(text)
-        discord_bot.send_message(text)
-
-    if test:
-        #print "get_final" function
-        print(text)
 
 
 if __name__ == '__main__':
@@ -341,35 +262,4 @@ if __name__ == '__main__':
     except KeyError:
         my_timezone='America/New_York'
 
-    game_timezone='America/New_York'
-    bot_main("init")
-    sched = BlockingScheduler(job_defaults={'misfire_grace_time': 15*60})
-
-    #power rankings:                     tuesday evening at 6:30pm local time.
-    #matchups:                           thursday evening at 7:30pm east coast time.
-    #close scores (within 15.99 points): monday evening at 6:30pm east coast time.
-    #trophies:                           tuesday morning at 7:30am local time.
-    #score update:                       friday, monday, and tuesday morning at 7:30am local time.
-    #score update:                       sunday at 4pm, 8pm east coast time.
-
-    sched.add_job(bot_main, 'cron', ['get_power_rankings'], id='power_rankings',
-        day_of_week='tue', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=my_timezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_matchups'], id='matchups',
-        day_of_week='thu', hour=19, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=game_timezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_close_scores'], id='close_scores',
-        day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=game_timezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_final'], id='final',
-        day_of_week='tue', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=my_timezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1',
-        day_of_week='fri,mon', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
-        timezone=my_timezone, replace_existing=True)
-    sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard2',
-        day_of_week='sun', hour='16,20', start_date=ff_start_date, end_date=ff_end_date,
-        timezone=game_timezone, replace_existing=True)
-
-    print("Ready!")
-    sched.start()
+    bot_main("get_final")
