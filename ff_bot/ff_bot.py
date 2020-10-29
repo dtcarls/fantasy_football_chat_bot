78e6684b8913dfc608fb5732c6632772a97ea7c9
Sean Gallardo <sdvgallardo@gmail.com>
2020-10-29 18:48:46 -0500

    Fix problem with the word "Matchup" not appearing in the message

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -149,7 +149,7 @@ def get_matchups(league, week=None):
     score = ['%s(%s-%s) vs %s(%s-%s)' % (i.home_team.team_name, i.home_team.wins, i.home_team.losses,
              i.away_team.team_name, i.away_team.wins, i.away_team.losses) for i in matchups
              if i.away_team]
-    text = ['Matchups'] + score + random_phrase()
+    text = ['Matchups '] + score + random_phrase()
     return '\n'.join(text)
 
 def get_close_scores(league, week=None):
