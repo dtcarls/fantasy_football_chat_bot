9d70ac54c41dacea29530420b1014d9297302894
dtcarls <deantcarlson@gmail.com>
2022-09-19 13:51:05 -0400

    rename ff_bot to gamedaybot

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 ff_bot/ff_bot.py
+worker: python3 gamedaybot/ff_bot.py
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='ff_bot',
+    name='gamedaybot',
 
-    packages=['ff_bot'],
+    packages=['gamedaybot'],
 
     include_package_data=True,
 
