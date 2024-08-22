9ed1afb04e20ff2fce5e6b10c44d8b4781ba3cc5
dtcarls <deantcarlson@gmail.com>
2024-08-21 20:28:50 -0400

    add power rankings math to utils

--- gamedaybot/utils/util.py
+++ gamedaybot/utils/util.py
@@ -23,6 +23,41 @@ def str_to_bool(check: str) -> bool:
         return False
 
 
+def add_matrix(X, Y):
+    '''Adds two matrices'''
+    result = [[0.0 for x in range(len(X))] for y in range(len(X))]
+
+    for i in range(len(X)):
+
+        # iterate through columns
+        for j in range(len(X)):
+            result[i][j] = X[i][j] + Y[i][j]
+
+    return result
+
+
+def square_matrix(X):
+    '''Squares a matrix'''
+    result = [[0.0 for x in range(len(X))] for y in range(len(X))]
+
+    # iterate through rows of X
+    for i in range(len(X)):
+
+        # iterate through columns of X
+        for j in range(len(X)):
+
+            # iterate through rows of X
+            for k in range(len(X)):
+                result[i][j] += X[i][k] * X[k][j]
+
+    return result
+
+
+def two_step_dominance(X):
+    '''Returns result of two step dominance formula'''
+    matrix = add_matrix(square_matrix(X), X)
+    result = [sum(x) for x in matrix]
+    return result
 
 
 def str_limit_check(text: str, limit: int) -> List[str]:
